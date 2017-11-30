1d6f5fc15c7cedb14c2f4f64de8553c7fd1f30cf
Chris Bianca <me@chrisbianca.com>
2017-11-30 12:21:02 +0000

    [android] Remove unused RNFirebase packages

--- android/app/src/main/java/com/invertase/rnfirebasestarter/MainApplication.java
+++ android/app/src/main/java/com/invertase/rnfirebasestarter/MainApplication.java
@@ -15,16 +15,10 @@ import com.facebook.soloader.SoLoader;
 
 import io.invertase.firebase.RNFirebasePackage;
 // optional packages - add/remove as appropriate
-import io.invertase.firebase.admob.RNFirebaseAdMobPackage; //Firebase AdMob
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage; // Firebase Analytics
 import io.invertase.firebase.auth.RNFirebaseAuthPackage; // Firebase Auth
-import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage; // Firebase Remote Config
-import io.invertase.firebase.crash.RNFirebaseCrashPackage; // Firebase Crash Reporting
 import io.invertase.firebase.database.RNFirebaseDatabasePackage; // Firebase Realtime Database
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage; // Firebase Firestore
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // Firebase Cloud Messaging
-import io.invertase.firebase.perf.RNFirebasePerformancePackage; // Firebase Performance
-import io.invertase.firebase.storage.RNFirebaseStoragePackage; // Firebase Storage
 
 import java.util.Arrays;
 import java.util.List;
@@ -48,16 +42,10 @@ public class MainApplication extends Application implements ReactApplication {
         new FBSDKPackage(mCallbackManager),
         new RNFirebasePackage(),
         // add/remove these packages as appropriate
-        new RNFirebaseAdMobPackage(),
         new RNFirebaseAnalyticsPackage(),
         new RNFirebaseAuthPackage(),
-        new RNFirebaseRemoteConfigPackage(),
-        new RNFirebaseCrashPackage(),
         new RNFirebaseDatabasePackage(),
-        new RNFirebaseFirestorePackage(),
-        new RNFirebaseMessagingPackage(),
-        new RNFirebasePerformancePackage(),
-        new RNFirebaseStoragePackage()
+        new RNFirebaseFirestorePackage()
       );
     }
 
